accd2623f5c157ba5367b92092702bdcb76962ec
Ziming Liao <lzimingliao@gmail.com>
2026-04-27 16:39:20 +0800

    class019

---
+++ fast_io.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 1) + (x << 3) + (ch - '0');
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+int main() {
+    int x;
+    x = read();
+    writeln(x);
+    return 0;
+}
